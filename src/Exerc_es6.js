c4a488d30b349583dd27d6bf0fe73284faf45d2a
aleoteixeira <aleoteixeira@outlook.com>
2025-02-16 15:06:10 -0300

    Subir exercio de es6

---
+++ src/Exerc_es6.js
@@ -0,0 +1,15 @@
+const alunos = [
+  { nome: "João", nota: 7 },
+  { nome: "Maria", nota: 5 },
+  { nome: "Pedro", nota: 8 },
+  { nome: "Ana", nota: 6 },
+  { nome: "Carlos", nota: 4 },
+  { nome: "Julia", nota: 9 }
+];
+
+function filtrarAprovados(alunos) {
+  return alunos.filter(aluno => aluno.nota >= 6);
+}
+
+const aprovados = filtrarAprovados(alunos);
+console.log(aprovados);
